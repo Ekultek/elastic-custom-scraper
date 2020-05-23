baa7f3da4d0eb4ae3060c906c1ad21d8f0734cf3
19201 <40706508+19201@users.noreply.github.com>
2020-05-23 14:52:34 +0700

    Some updates

--- src/requester.py
+++ src/requester.py
@@ -1,47 +1,66 @@
 import requests
-import pandas as pd
-
-headers = {
-    'accept': "application/json",
-    'apikey': "jgmfGzQL6sTuupwAqDK3jiKJXy8YDw9z"
-    }
 
 writetofile = input("Name of file to write too? \n\n")
 siteorcsv = input("Check out a specific site or load default CSV? Please, no http:// or www if entering site. Simply enter 'default' to use default CSV \n\n")
+
 
 def write(data, filename=writetofile):
     f = open(filename, "a")
     f.write(data + '\r\n')
     f.close()
 
-if siteorcsv == "default":
-    df = pd.read_csv('sitelist.csv')
-else:
-    df = {}
-    df["API_SITE"] = siteorcsv
-    print(siteorcsv)
 
-for specurl in df["API_SITE"]:
-    if len(specurl) < 2:
-        specurl = siteorcsv
-
-    api_endpoint = "https://api.securitytrails.com/v1/domain/" + specurl + "/subdomains"
-    response = requests.request("GET", api_endpoint, headers=headers, timeout=7)
-    json_data = response.json()
-    count = 0
-    print(json_data)
-    for subdomain in json_data["subdomains"]:
-        final_count = len(json_data["subdomains"])
-        request_subdomain = "http://" + subdomain + "." + specurl
-        print(request_subdomain + " " + str(count) + '/' + str(final_count))
-        count = count + 1
-        try:
-            r = requests.head(request_subdomain, timeout=3)
-            status_result = str(r.status_code) + "," + request_subdomain
-            print(status_result + "\n")
-            write(status_result)
-        except:
-            print("TIMEOUT!")
-            continue
+def write_array(lines, filename):
+    for line in lines:
+        f = open(filename, "a")
+        f.write(line + '\r\n')
+        f.close()
 
 
+def get_domain_status(url):
+    return requests.head(url, timeout=3).status_code
+
+
+def get_request(url, c_headers):
+    return requests.request("GET", url, headers=c_headers, timeout=7)
+
+
+def file_to_array(filename):
+    with open(filename) as file_in:
+        lines = []
+        for line in file_in:
+            lines.append(line.rstrip())
+    print('LOADED ' + str(len(lines) + ' DOMAINS'))
+    return lines
+
+
+def get_domains_from_file(u_input):
+    switcher = {
+        'default': 'sitelist.txt',
+        'test': 'this_file.txt',
+    }
+    return file_to_array(switcher.get(u_input, u_input))  # switcher.get(input, default_response)
+
+
+def loop_and_check_domains(domain_list):
+    for domain in domain_list:
+        response = get_request("https://api.securitytrails.com/v1/domain/" + domain + "/subdomains",
+                               { 'accept': "application/json", 'apikey': "jgmfGzQL6sTuupwAqDK3jiKJXy8YDw9z" })
+        json_data = response.json()
+        print(json_data)
+        write_array(json_data["subdomains"], 'subdomains/'+domain+'.txt')
+        for subdomain in json_data["subdomains"]:
+            final_count = len(json_data["subdomains"])
+            request_subdomain = "http://" + subdomain + "." + domain
+            print(request_subdomain + " " + str(count) + '/' + str(final_count))
+            count = count + 1
+            try:
+                status_result = str(get_domain_status(request_subdomain)) + "," + request_subdomain
+                print(status_result + "\n")
+                write(status_result)
+            except:
+                print("TIMEOUT!")
+                continue
+
+
+loop_and_check_domains(get_domains_from_file(siteorcsv))
