e899e49c9f2ac0e2fc95eef09b8556f432476fc8
Dan Ehrlich <dan.ehrlich@12security.com>
2021-04-15 13:21:20 -0500

    commented out 2

--- src/elasticdump/download_indices.sh
+++ src/elasticdump/download_indices.sh
@@ -6,7 +6,7 @@ url="192.168.0.1"
 port="9200"
 
 while read -r line; do
- elasticdump --input=https://$user:$password@$url:$port/$line --output=$line.json --limit 500 --sourceOnly --noRefresh --fileSize 400 MB
+ elasticdump --input=https://$url:$port/$line --output=$line.json --limit 500 --sourceOnly --noRefresh --fileSize 400 MB
 done < index_names.txt
 
 #while read -r line; do
